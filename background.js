b8c52c3f978a4287bf721defa9815b46e88b1f92
Joey You <57522684+joeyyou@users.noreply.github.com>
2022-08-12 17:12:54 +0800

    workState Badge

--- background.js
+++ background.js
@@ -1,3 +1,24 @@
+let workState = true
+
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Hypernotes 已加载');
+  displayState(workState)
 })
+
+chrome.action.onClicked.addListener((tab) => {
+  workState = !workState
+  displayState(workState)
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ['js/content.js']
+  });
+});
+
+function displayState(state) {
+  if (state) {
+    console.log('Hypernotes 已启动');
+    chrome.action.setBadgeText({ text: 'ON' });
+  } else {
+    console.log('Hypernotes 已关闭');
+    chrome.action.setBadgeText({ text: 'OFF' });
+  }
+}
