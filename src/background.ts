8a0beaedaf67b4e1d397598223d136cf551f1527
Joey You <57522684+joeyyou@users.noreply.github.com>
2022-08-16 11:01:21 +0800

    update content.ts

--- src/background.ts
+++ src/background.ts
@@ -1,17 +1,19 @@
-let workState = true
+let workState = true;
 
 chrome.runtime.onInstalled.addListener(() => {
-  displayState(workState)
+  displayState(workState);
 })
 
 chrome.action.onClicked.addListener((tab) => {
-  workState = !workState
-  displayState(workState)
+  workState = !workState;
+  displayState(workState);
+  chrome.tabs.sendMessage(tab.id, { state: workState });
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['js/content.js']
   });
 });
+
 
 function displayState(state) {
   if (state) {
--- src/js/content.ts
+++ src/js/content.ts
@@ -2,3 +2,34 @@
 // https://stackoverflow.com/questions/39610205/how-to-make-side-panel-in-chrome-extension
 
 console.log('[content.js] loaded');
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+  let iframe = document.getElementById("sidebar")
+  if (msg.state) {
+    openPanel(iframe);
+  } else {
+    closePanel(iframe);
+  }
+});
+
+if (document.getElementById("sidebar") === null || document.getElementById("sidebar") === undefined) {
+  let iframe = document.createElement('iframe');
+  iframe.setAttribute("id", "sidebar");
+  iframe.style.background = "green";
+  iframe.style.height = "100%";
+  iframe.style.width = "0px";
+  iframe.style.position = "fixed";
+  iframe.style.top = "0px";
+  iframe.style.right = "0px";
+  iframe.style.zIndex = "9000000000000000000";
+  iframe.frameBorder = "none";
+
+  document.body.appendChild(iframe);
+  
+}
+
+function openPanel(iframe) {
+  iframe.style.width = "400px";
+}
+function closePanel(iframe) {
+  iframe.style.width = "0px";
+}
