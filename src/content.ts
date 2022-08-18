7b905925af94cfaab2f0b197e15730815e3aa019
Joey You <57522684+joeyyou@users.noreply.github.com>
2022-08-18 14:28:05 +0800

    createFloatBtn

--- src/content.ts
+++ src/content.ts
@@ -10,7 +10,7 @@ console.log('[content.js] loaded');
 // 与 service worker 通信
 chrome.runtime.onMessage.addListener(function (msg, sender) {
   let iframe = document.getElementById("sidebar");
-  console.log(iframe);
+  // console.log(iframe);
   if (msg.state) {
     openPanel(iframe);
   } else {
@@ -22,36 +22,60 @@ chrome.runtime.onMessage.addListener(function (msg, sender) {
 if (document.getElementById("sidebar") === null || document.getElementById("sidebar") === undefined) {
   let iframe = createFrame();
   document.body.appendChild(iframe);
+
+  // 在iframe中插入html
   iframe.contentWindow.document.open();
   iframe.contentWindow.document.write(html);
+  iframe.contentWindow.document.close();
+
+  // 在iframe中插入css
   let style = `<style>${css}</style>`
   iframe.contentDocument.body.innerHTML = iframe.contentDocument.body.innerHTML + style;
-  iframe.contentWindow.document.close();
-  console.log(iframe);
+
+  // 显示iframe
+  openPanel(iframe);
 }
 
-// 监听选择文本事件
+// DOM 没有选择文本事件，通过监听按键事件来间接实现
 document.onmouseup = handleSelection;
+document.onmousedown = removeFloatBtn;
 
 // 处理选择文本，高亮或标注
 function handleSelection(event) {
-  let selection = document.getSelection().toString();
-  if (selection !== '') {
+  let selection = document.getSelection();
+  let selectedText = selection.toString();
+  if (selectedText !== '') {
     console.log(selection);
+    console.log(event);
+    // 创建按钮
+    removeFloatBtn();
+    let floatBtn = createFloatBtn(event.screenX, event.screenY);
+    document.body.appendChild(floatBtn);
+  }else{
+    console.log("未选择文本");
+  }
+}
+
+// 清除浮动按钮
+function removeFloatBtn() {
+  let floatBtn = document.getElementById("float-btn");
+  if (floatBtn) {
+    document.body.removeChild(floatBtn);
   }
 }
 
 // 创建侧边栏方法
-function createFrame(){
+function createFrame() {
   let iframe = document.createElement('iframe');
   iframe.setAttribute("id", "sidebar");
   iframe.style.position = "fixed";
   iframe.style.height = "100%";
+  iframe.style.width = "0px";
   iframe.style.top = "0px";
   iframe.style.right = "0px";
   iframe.style.border = "none";
   iframe.style.zIndex = "999";
-  
+
   return iframe;
 }
 
@@ -60,4 +84,24 @@ function openPanel(iframe) {
 }
 function closePanel(iframe) {
   iframe.style.width = "0px";
+}
+
+// 创建浮动按钮
+function createFloatBtn(x, y) {
+  let button = document.createElement("div");
+  button.id = "float-btn";
+  button.style.position = "absolute";
+  button.style.width = "30px";
+  button.style.height = "30px";
+  button.style.left = x+"px";
+  button.style.top = y+"px";
+  button.style.border = "none";
+  button.style.cursor = "pointer";
+
+  let img = document.createElement("img");
+  let imgURL = chrome.runtime.getURL("img/icon.png");
+  img.setAttribute("src", imgURL);
+
+  button.appendChild(img);
+  return button;
 }
